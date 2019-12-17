e1c683f7198b77d49625765b387a89ba805f00ea
Calvin Kopitz <calvinkopitz@gmail.com>
2019-12-17 12:49:31 -0500

    Keyboard controls added, but they control everyone at once

--- Attract your friends/Assets/Scripts/InputManager.cs
+++ Attract your friends/Assets/Scripts/InputManager.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public static class InputManager{
     public static float HorizontalAxis(int playerNum){
-        return Input.GetAxis("Horizontal_Xbox" + playerNum);;
+        return Mathf.Clamp(Input.GetAxis("Horizontal_Xbox" + playerNum) + Input.GetAxis("Horizontal"), -1, 1);
     }
 
     public static bool PullButton(int playerNum){
-        return Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false;
+        return (Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false) || Input.GetKey(KeyCode.DownArrow);
     }
 
     public static bool PushButton(int playerNum){
-        return Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false;
+        return (Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false) || Input.GetKey(KeyCode.UpArrow);
     }
 
     public static bool JumpButton(int playerNum){
-        return Input.GetButton("Jump_Xbox" + playerNum);
+        return Input.GetButton("Jump_Xbox" + playerNum) || Input.GetButton("Jump");
     }
 }
