cd60a503bad387d2d24eb827b9e39f5f7ceeaf16
Calvin Kopitz <calvinkopitz@gmail.com>
2019-12-22 15:19:44 -0500

    Fixes some players being unable to jump using the Y button

--- Attract your friends/Assets/Scripts/InputManager.cs
+++ Attract your friends/Assets/Scripts/InputManager.cs
@@ -3,34 +3,18 @@ using System.Collections;
 
 public static class InputManager{
     public static float HorizontalAxis(int playerNum){
-        float horizontalInput = 0;
-        if (playerNum == 1) {
-            horizontalInput = Input.GetAxis("Horizontal");
-        }
-        return Mathf.Clamp(Input.GetAxis("Horizontal_Xbox" + playerNum) + horizontalInput, -1, 1);
+        return Input.GetAxis("Horizontal_Xbox" + playerNum);
     }
 
     public static bool PullButton(int playerNum){
-        bool pullInput = false;
-        if (playerNum == 1) {
-            pullInput = Input.GetKey(KeyCode.K);
-        }
-        return (Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false) || pullInput;
+        return Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false;
     }
 
     public static bool PushButton(int playerNum){
-        bool pushInput = false;
-        if (playerNum == 1) {
-            pushInput = Input.GetKey(KeyCode.J);
-        }
-        return (Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false) || pushInput;
+        return Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false;
     }
 
     public static bool JumpButton(int playerNum){
-        bool jumpInput = false;
-        if (playerNum == 1) {
-            jumpInput = Input.GetButton("Jump");
-        }
-        return Input.GetButton("Jump_Xbox" + playerNum) || jumpInput;
+        return Input.GetButton("Jump_Xbox" + playerNum);
     }
 }
