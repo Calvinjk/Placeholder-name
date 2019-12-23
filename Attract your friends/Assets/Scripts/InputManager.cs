68dd03a35133de68b8d1877661b372eaa4e19fae
Calvin Kopitz <calvinjk@umich.edu>
2019-12-22 21:42:58 -0500

    Re-mapped keyboard controlls to explicit Inputs

--- Attract your friends/Assets/Scripts/InputManager.cs
+++ Attract your friends/Assets/Scripts/InputManager.cs
@@ -7,18 +7,30 @@ public static class InputManager{
         if (playerNum == 1){
             horizontalInput = Input.GetAxis("Horizontal_Keyboard");
         }
-        return Input.GetAxis("Horizontal_Xbox" + playerNum);
+        return Mathf.Clamp(Input.GetAxis("Horizontal_Xbox" + playerNum) + horizontalInput, -1, 1);
     }
 
     public static bool PullButton(int playerNum){
-        return Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false;
+        bool pullInput = false;
+        if (playerNum == 1) {
+            pullInput = Input.GetButton("Pull_Keyboard");
+        }
+        return (Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false) || pullInput;
     }
 
     public static bool PushButton(int playerNum){
-        return Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false;
+        bool pushInput = false;
+        if (playerNum == 1) {
+            pushInput = Input.GetButton("Push_Keyboard");
+        }
+        return (Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false) || pushInput;
     }
 
     public static bool JumpButton(int playerNum){
-        return Input.GetButton("Jump_Xbox" + playerNum);
+        bool jumpInput = false;
+        if (playerNum == 1) {
+            jumpInput = Input.GetButton("Jump_Keyboard");
+        }
+        return Input.GetButton("Jump_Xbox" + playerNum) || jumpInput;
     }
 }
