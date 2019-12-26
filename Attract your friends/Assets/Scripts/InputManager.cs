06b9067d292144db70dabc3d9a33353ef9cbb5be
Calvin Kopitz <calvinjk@umich.edu>
2019-12-26 09:50:01 -0500

    Tutorial scene and rounded platforms (#37)
    * Fixes some players being unable to jump using the Y button
    
    * Added explicit Keyboard inputs
    
    * Re-mapped keyboard controlls to explicit Inputs
    
    * Rounded platforms prefabs, added tutorial scene, deleted unnecessart InputManager state

--- Attract your friends/Assets/Scripts/InputManager.cs
+++ Attract your friends/Assets/Scripts/InputManager.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public static class InputManager{
     public static float HorizontalAxis(int playerNum){
         float horizontalInput = 0;
-        if (playerNum == 1) {
-            horizontalInput = Input.GetAxis("Horizontal");
+        if (playerNum == 1){
+            horizontalInput = Input.GetAxis("Horizontal_Keyboard");
         }
         return Mathf.Clamp(Input.GetAxis("Horizontal_Xbox" + playerNum) + horizontalInput, -1, 1);
     }
@@ -13,7 +13,7 @@ public static class InputManager{
     public static bool PullButton(int playerNum){
         bool pullInput = false;
         if (playerNum == 1) {
-            pullInput = Input.GetKey(KeyCode.K);
+            pullInput = Input.GetButton("Pull_Keyboard");
         }
         return (Input.GetAxis("LTrigger_Xbox" + playerNum) > 0 ? true : false) || pullInput;
     }
@@ -21,7 +21,7 @@ public static class InputManager{
     public static bool PushButton(int playerNum){
         bool pushInput = false;
         if (playerNum == 1) {
-            pushInput = Input.GetKey(KeyCode.J);
+            pushInput = Input.GetButton("Push_Keyboard");
         }
         return (Input.GetAxis("RTrigger_Xbox" + playerNum) > 0 ? true : false) || pushInput;
     }
@@ -29,7 +29,7 @@ public static class InputManager{
     public static bool JumpButton(int playerNum){
         bool jumpInput = false;
         if (playerNum == 1) {
-            jumpInput = Input.GetButton("Jump");
+            jumpInput = Input.GetButton("Jump_Keyboard");
         }
         return Input.GetButton("Jump_Xbox" + playerNum) || jumpInput;
     }
