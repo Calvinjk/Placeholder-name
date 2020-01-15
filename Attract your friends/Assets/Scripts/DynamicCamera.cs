fffc08d5f25baaa24d7f027cfc5556118bcf8197
Calvin Kopitz <calvinjk@umich.edu>
2020-01-15 07:52:45 -0500

    Begging the dynamic camera work, WIP

--- Attract your friends/Assets/Scripts/DynamicCamera.cs
+++ Attract your friends/Assets/Scripts/DynamicCamera.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DynamicCamera : MonoBehaviour {
-    private Gameobject[] players;
+    private GameObject[] players;   // An array that holds all of the players in the scene
+
+    private float leftBound;
+    private float rightBound;
+    private float bottomBound;
+    private float topBound;
 
     void Awake(){
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     void FixedUpdate(){
+        // We need to figure out what things we want in the camera that would be at the edges of the camera
+        DetermineCameraEdges();
 
+        // TODO - Determine margins and actually set the camera.
+    }
+
+    void DetermineCameraEdges() {
+        leftBound = Mathf.Infinity;
+        rightBound = -Mathf.Infinity;
+        bottomBound = Mathf.Infinity;
+        topBound = -Mathf.Infinity;
+        
+        for (int i = 0; i < players.Length; ++i) {
+            Vector2 pos = players[i].transform.position;
+            if (pos.x < leftBound) { leftBound = pos.x; }
+            if (pos.x > rightBound) { rightBound = pos.x; }
+            if (pos.y < bottomBound) { bottomBound = pos.y; }
+            if (pos.y > topBound) { topBound = pos.y; }
+        }
     }
 }
